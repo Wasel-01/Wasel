43ae979d1164a2dfeb2c625563cdf52af875c5f5
Wasel-01 <laith.nassar.70@gmail.com>
2025-10-30 08:26:25 +0300

    t

---
+++ vite.config.simple.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+  },
+})
